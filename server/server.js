ef34a77bb3a796adb39ad9961dd39f48d735825f
Infi-Knight <soniravi829@gmail.com>
2017-07-07 13:14:01 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+
+});
+app.listen(port, process.env.IP () => {
+  console.log(`Server up and running on port ${port}`);
+});
