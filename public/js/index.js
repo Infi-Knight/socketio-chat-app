632668a498f947210900732bdebc95218e938a20
Infi-Knight <soniravi829@gmail.com>
2017-07-09 20:41:16 +0530

    add messaging system

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var socket = io();
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('newMessage', function(msg) {
+    console.log('New Message', msg);
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
--- server/server.js
+++ server/server.js
@@ -15,13 +15,41 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit emits an event to a single connection
+  // first argument is the name of the event we want to emit
+  // the listener's name on client side should match this name
+  // Second argument is the data we wanna pass
+  // socket.emit('newMessage', {
+  //   text: 'Hasta la Vista, Baby'
+  // });
+
+  // io.emit broadcasts the event to all the connections
+  socket.on('createMessage', (message) => {
+    console.log('User: ', message);
+    // We recieved a message from the user 
+    // Now broadcast the message to all the users
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  });
+
   socket.on('disconnect', () => {
     console.log('User Disconnected');
   });
 });
 
+
+
+
+
 app.get('/', (req, res) => {
 
+});
+
+app.get('/test', (req, res) => {
+  res.send('Hii');
 });
 
 server.listen(port, process.env.IP, () => {
